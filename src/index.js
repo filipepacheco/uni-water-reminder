39a03c0393f972b7e463c9a2916c89394e851559
Lipe <lip3pacheco@gmail.com>
2023-11-07 17:03:53 -0300

    Amplify pull

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Assuming this is the path to your App component
+
+const root = document.getElementById('root');
+const reactRoot = ReactDOM.createRoot(root);
+
+reactRoot.render(<App />);
